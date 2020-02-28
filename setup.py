41764696c45dbf1e8ead19579b2eb10758420eae
jcyang <34616545+KevinBastianYang@users.noreply.github.com>
2020-02-28 14:12:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='Smith_Waterman_Implementation',
+setup(name='SW',
       version='1.0.0',
       description='Smith-Waterman Algorithm',
-      url='http://github.com/KevinBastianYang/Smith_Waterman_Implementation',
+      url='http://github.com/KevinBastianYang/SW',
       author='Junchen Yang',
       author_email='junchen.yang@yale.edu',
       license='GPL',
-      packages=['Smith_Waterman_Implementation'],
+      packages=['SW'],
       zip_safe=False)
